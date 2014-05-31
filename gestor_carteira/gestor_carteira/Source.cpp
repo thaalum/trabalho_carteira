7ddb305c3d754346c83461a46cfb35c55c3c8a6c
thaalum <fabioandreosp@gmail.com>
2014-05-31 18:21:10 +0100

    fab

--- gestor_carteira/gestor_carteira/Source.cpp
+++ gestor_carteira/gestor_carteira/Source.cpp
@@ -1,24 +1,60 @@
 #include <stdio.h>
 #include <iostream>
 using namespace std;
+struct data
+{
+	int dia;
+	int mes;
+	int ano;
+};
+struct carteira
+{
+	char tipo;
+	float valor;
+	struct data data1;
+};
+int posicao_vector = 0;
+carteira v[9999];
+void menu();
+void opcao1();
+void opcao2();
+void opcao3();
+void opcao4();
+void opcao5();
+void opcao6();
+void opcao7();
+void opcao8();
+void opcao9();
+void opcao10();
+void opcao11();
+void opcao12();
 
 void main()
 {
-	struct data
-	{
-		int dia;
-		int mes;
-		int ano;
-	};
-	struct carteira
-	{
-		char tipo;
-		float valor;
-		struct data data1;
-	};
 	int opcao_escolhida;
-	carteira v[9999];
-	int posicao_vector = 0;
+	menu();
+	cin >> opcao_escolhida;
+	while (opcao_escolhida != 12)
+	{
+		switch (opcao_escolhida)
+		{
+			case 1: opcao1(); break;
+			case 2: opcao2(); break;
+			case 3: opcao3(); break;
+			case 5: opcao5(); break;
+			menu();
+			cin >> opcao_escolhida;
+		};
+	};
+};
+
+
+
+
+
+
+void menu()
+{
 	cout << "1 - INSERIR REGISTO NOVO\n";
 	cout << "2 - LISTAR DESPESAS\n";
 	cout << "3 - LISTAR RECEITAS\n";
@@ -32,36 +68,56 @@ void main()
 	cout << "11 - RECEITAS ACIMA DE:\n";
 	cout << "12 - SAIR\n";
 	cout << "ESCOLHA UMA OPCAO: ";
-	cin >> opcao_escolhida;
-	while (opcao_escolhida != 12)
+};
+
+void opcao1()
+{
+	cout << "Despesa (d) ou receita (r): ";
+	cin >> v[posicao_vector].tipo;
+	cout << "Valor:";
+	cin >> v[posicao_vector].valor;
+	cout << "\n Insira, a data de hoje: ";
+	cout << "\n Dia:";
+	cin >> v[posicao_vector].data1.dia;
+	cout << "\n Mes:";
+	cin >> v[posicao_vector].data1.mes;
+	cout << "\n Ano:";
+	cin >> v[posicao_vector].data1.ano;
+	posicao_vector++;
+};
+
+void opcao2()
+{
+	int i;
+	for (i = 0; i <= posicao_vector; i++)
 	{
-		switch (opcao_escolhida)
+		if (v[i].tipo == 'd')
+			cout << "Numero de registo:" << i << "\n Quantia:" << v[i].valor << "\n Data:" << v[i].data1.dia << "  " << v[i].data1.mes << "  " << v[i].data1.ano << "\n \n";
+	};
+};
+
+void opcao3()
+{
+	int i;
+	for (i = 0; i <= posicao_vector; i++)
+	{
+		if (v[i].tipo == 'r')
+			cout << "Numero de registo:" << i << "\n Quantia:" << v[i].valor << "\n Data:" << v[i].data1.dia << "  " << v[i].data1.mes << "  " << v[i].data1.ano << "\n \n";
+	};
+};
+
+void opcao5()
+{
+	int i;
+	float media = 0;
+	int cont;
+	for (i = 0; i <= posicao_vector; i++)
+	{
+		if (v[i].tipo == 'd')
 		{
-			case 1:
-				cout << "Despesa d ou receita r: \n";
-				cin >>  v[posicao_vector].tipo;
-				cout << "Valor:";
-				cin >> v[posicao_vector].valor;
-				cout << "\n Insira, a data de hoje: ";
-				cout << "\n Dia:";
-				cin >>  v[posicao_vector].data1.dia;
-				cout << "\n Mes:";
-				cin >> v[posicao_vector].data1.mes;
-				cout << "\n Ano:";
-				cin >>  v[posicao_vector].data1.ano;
-				posicao_vector++;
-				break;
-			case 2:
-				int i;
-				for (i = 0; i <= posicao_vector; i++)
-				{
-					if (v[i].tipo == 'd')
-						cout << "Registo:"<< i<< "\n Quantia:"<< v[i].valor<< "Data:"<< v[i].data1.dia,  v[i].data1.mes,  v[i].data1.ano;
-				};
-				break;
+			media = media + v[i].valor;
+			cont++;
 		}
-
+		cout << "Media de despesas:" << media / cont;
 	}
-}
-	
-
+};
